31edf06394f8ef7fce89e2dbb937952a9c8785aa
Michela Tjan <michelatjan@gmail.com>
2025-04-11 15:03:21 -0400

    bug: Bypass streamlit welcome email

--- ACUNAO-AI.py
+++ ACUNAO-AI.py
@@ -1,11 +1,19 @@
 import os, sys
 
-os.environ["STREAMLIT_EMAIL"] = "dummy@example.com"
-os.environ["STREAMLIT_HEADLESS"] = "true"
-
 import streamlit
 
 import streamlit.web.cli as stcli
+import shutil
+
+streamlit_path = os.path.expanduser("~/.streamlit")
+
+if not os.path.exists(streamlit_path):
+        os.makedirs(streamlit_path)
+
+creds = os.path.join(os.path.dirname(os.path.realpath(__file__)),"utils/data/credentials.toml")
+
+if not os.path.isfile(os.path.join(streamlit_path, "credentials.toml")):
+    shutil.copy2(creds, streamlit_path)
 
 
 def resolve_path(path):
@@ -22,6 +30,6 @@ if __name__ == "__main__":
         resolve_path("utils/app.py"),
         "--server.port", str(port),
         "--global.developmentMode=false",
-        "os.devnull"
+        # "--server.headless", "true"
     ]
     sys.exit(stcli.main())
